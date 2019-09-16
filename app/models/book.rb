893811f33e7b72e50b64c377235ca0d11bfa2895
Sian Stone <sian.m.stone@gmail.com>
2019-09-16 13:26:02 +0100

    login form on landing page basic styles, add_book basic form complete

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ApplicationRecord
   has_many :authors_books
   has_many :users_books
   has_many :authors, through: :authors_books
+
+  mount_uploader :cover, PhotoUploader
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/create'
+  get 'books/edit'
+  get 'books/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
